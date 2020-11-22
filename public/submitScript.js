feaca801862a0232cea68991f40c3ae32a02b08d
carsonkaylor1 <carsonkaylor@gmail.com>
2020-11-22 10:35:18 -0500

    submiterrorb

---
+++ public/submitScript.js
@@ -0,0 +1,27 @@
+let myAccount = sessionStorage.getItem('account');
+
+if(!myAccount){
+      window.location = './home';
+}
+
+firebase.auth().onAuthStateChanged(function(user){
+      if(!user){
+            window.location = './signinerror';
+      }
+    });
+
+
+function checkAccountID(){
+      var sellerRef = firebase.firestore().collection("sellers");
+      sellerRef.where('email', '==', email).get()
+      .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            var sellerConnectedAccountIDValue = doc.data().connectedAccountId;
+            
+            if (!sellerConnectedAccountIDValue){
+                  window.location = '/submiterrorb'
+            }
+            
+        });
+      })
+}
