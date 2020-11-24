9175002418dd0cbfb9ddaa8254a899aaaf2ae9e9
carsonkaylor1 <carsonkaylor@gmail.com>
2020-11-24 16:15:53 -0500

    api

--- public/script.js
+++ public/script.js
@@ -27,6 +27,20 @@ if (elmButton) {
   );
 }
 
+fetch('/api-key', {
+  method: "GET",
+  headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+        },
+}).then(res =>  res.json())
+.then(data => {
+  const{
+    result
+  } = data;
+  console.log(data)
+})
+
 function checkIfLoggedIn(){
   const firebasePromise = new Promise((resolve, reject) => {
     firebase.auth().onAuthStateChanged(function(user){
