4436d42ebc18aa03965242aaf53fc0ba40dba449
carsonkaylor1 <carsonkaylor@gmail.com>
2021-06-14 19:36:13 -0400

    simplify initialize firebase

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAwVyTuwLna3gN798rQA406RXIWZ6vU5iA",
+    authDomain: "waltz-c694e.firebaseapp.com",
+    databaseURL: "https://waltz-c694e.firebaseio.com",
+    projectId: "waltz-c694e",
+    storageBucket: "waltz-c694e.appspot.com",
+    messagingSenderId: "305484219833",
+    appId: "1:305484219833:web:6923cf3e23025f971f6268",
+    measurementId: "G-B2971FFR0G"
+  };
+
+firebase.initializeApp(firebaseConfig);
